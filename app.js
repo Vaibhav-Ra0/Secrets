64ac24970f41da455f27e2bbfb0cbb9a61a45f58
vaibhav rao <me.vaibhavrao833@gmail.com>
2023-07-13 13:59:04 +0530

    added environment variable to keeps secrets safe

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -17,13 +18,11 @@ const userSchema = new mongoose.Schema({
     password : String
 })
 
-const secret = "ThisisOurLittleSecret.";
-
 // userSchema.plugin(encrypt,{secret : secret});  // this wiil encrypt our entire database
-
 //encrypt only certain fields
+//.env work here
 
-userSchema.plugin(encrypt,{secret: secret,encryptedFields:["password"]})
+userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]})
 
 
 const User = new mongoose.model("User",userSchema);
